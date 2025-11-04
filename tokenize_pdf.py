215478cecf604329e02c8ce1ebae0cbe9963e401
naza-campioni <56881474+naza-campioni@users.noreply.github.com>
2025-11-04 19:24:00 +0100

    Create tokenize_pdf.py

---
+++ tokenize_pdf.py
@@ -0,0 +1,19 @@
+from pypdf import PdfReader
+from tokenizer import tok_and_chunk
+
+def tokenize_pdf(pdf, model="text-embedding-3-small", max_tokens=500, overlap=50):
+  reader = PdfReader(pdf)
+  all_chunks = []
+  for page_num, page in enumerate(reader.pages):
+    text = page.extract_text()
+    page_chunks = tok_and_chunk(text, model=model, max_tokens=max_tokens, overlap=overlap)
+    # for i, chunk_text in enumerate(page_chunks):
+    #     all_chunks.append({
+    #         "page": page_num,
+    #         "chunk_id": i,
+    #         "text": chunk_text
+    #     })
+
+    for _, chunk_text in enumerate(page_chunks):
+        all_chunks.append(chunk_text)
+  return all_chunks
