900298b7a4be238daa04abf3b4fa415548932ba8
naza-campioni <56881474+naza-campioni@users.noreply.github.com>
2025-11-04 19:25:45 +0100

    Create query_index.py

---
+++ query_index.py
@@ -0,0 +1,12 @@
+from embedding import get_embedding
+
+def query_index(query, embeddings, k):
+  dim = embeddings.shape[1]
+  index = faiss.IndexFlatL2(dim)
+  index.add(embeddings)
+  query_emb = get_embedding(query).reshape(1, -1)
+  D, I = index.search(query_emb, k)
+
+  retrieved_text = list(embeddings[i] for i in I.flatten())
+  return retrieved_text
+  # retrieved_text = list(sentences[i] for i in I.flatten())
