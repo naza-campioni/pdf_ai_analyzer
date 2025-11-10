f5883d979db51b0b099d4730b4759eae262f8fef
naza-campioni <nazareno.campioni@gmail.com>
2025-11-10 19:47:38 +0100

    lang detect

--- functions/main.py
+++ functions/main.py
@@ -1,6 +1,7 @@
 # main function
 import os
 from openai import OpenAI
+from langdetect import detect
 
 from functions.embeddings import get_embedding
 from functions.sort_index import sort_index
@@ -18,9 +19,11 @@ def main(text, query, k=5):
   """
   my_key = os.getenv("OPENAI_API_KEY")
   client = OpenAI(api_key=my_key)
+  
+  lang = detect(query)
 
   token_pdf = tokenize_pdf(text)
   embeddings = get_embedding(token_pdf)
   retrieved_text = sort_index(query, token_pdf, embeddings, k=k)
-  answer = generate_answer(query, retrieved_text)
+  answer = generate_answer(query, retrieved_text, lang)
   return answer
