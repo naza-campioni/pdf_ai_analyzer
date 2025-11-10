4ea2e6cddf5b91b8b8c61f33230c80334adb4337
naza-campioni <nazareno.campioni@gmail.com>
2025-11-10 19:47:58 +0100

    lang detect

--- functions/generate_answer.py
+++ functions/generate_answer.py
@@ -1,7 +1,8 @@
 import os
 from openai import OpenAI
 
-def generate_answer(query, retrieved_text):
+
+def generate_answer(query, retrieved_text, language):
     """
     query: the user question
     retrieved_text: string containing the most relevant chunk(s)
@@ -9,9 +10,10 @@ def generate_answer(query, retrieved_text):
 
     my_key = os.getenv("OPENAI_API_KEY")
     client = OpenAI(api_key=my_key)
+    
 
     prompt = f"""
-    Your aim is to analyze the input and answer any input-specific questions using ONLY the information provided below.
+    Your aim is to analyze the input and answer any input-specific questions using ONLY the information provided below in the language {language}.
     If the information is not in the context, say you don't know. 
 
     
